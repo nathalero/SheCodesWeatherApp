1eda83a9eed0d97d58b9d928a04131c2f4cf0df5
nathalero <110853338+nathalero@users.noreply.github.com>
2022-09-20 15:07:05 +0200

    Units changing code

--- script.js
+++ script.js
@@ -29,7 +29,10 @@ function displayTemperature(response){;
     let windElement=document.querySelector("#wind");
     let dateElement=document.querySelector("#date");
     let iconElement=document.querySelector("#icon");
-    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+    celciusTemperature= response.data.main.temp;
+
+    temperatureElement.innerHTML = Math.round(celciusTemperature);
     cityElement.innerHTML= response.data.name;
     descriptionElement.innerHTML= response.data.weather[0].description;
     humidityElement.innerHTML=response.data.main.humidity;
@@ -56,6 +59,35 @@ function handleSubmit(event){
     search(cityInputElement.value);
 }
 
+function displayFahrenheitTemperature(event){
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+    celciusLink.classList.remove("active");
+    fahrenheitLink.classList.add("active");
+    let fahrenheitTemperature= (celciusTemperature * 9) / 5 + 32;
+    temperatureElement.innerHTML= Math.round(fahrenheitTemperature);
+}
+
+function displayCelciusTemperature(event){
+    event.preventDefault();
+    let temperatureElement=document.querySelector("#temperature");
+    celciusLink.classList.add("active");
+    fahrenheitLink.classList.remove("active");
+
+    temperatureElement.innerHTML=Math.round(celciusTemperature);
+}
+
+
+let celciusTemperature=null;
 
 let form= document.querySelector("#search-form");
 form.addEventListener("submit",handleSubmit);
+
+let fahrenheitLink=document.querySelector("#fahrenheit");
+fahrenheitLink.addEventListener("click",displayFahrenheitTemperature);
+
+let celciusLink=document.querySelector("#celcius");
+celciusLink.addEventListener("click",displayCelciusTemperature);
+
+
+search("Paris");
