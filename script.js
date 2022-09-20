26d004bfa58edd5912cfd3fd4f69bfb457d94469
nathalero <110853338+nathalero@users.noreply.github.com>
2022-09-20 12:51:42 +0200

    added weather API

---
+++ script.js
@@ -0,0 +1,15 @@
+function displayTemperature(response){;
+    let temperatureElement=document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement=document.querySelector("#description");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML= response.data.name;
+    descriptionElement.innerHTML= response.data.weather[0].description;
+}
+
+let city="paris"
+let apiKey = "8c48afa47a9a9c24f3500c7039d50aaa";
+let units = `metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+    
+axios.get(apiUrl).then(displayTemperature);
